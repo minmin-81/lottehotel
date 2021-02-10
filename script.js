bad036b5c36d5f4e7f638571de409d9735da8639
minmin <>
2021-02-10 12:29:33 +0900

    lotte hotel version1

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#letter_right h4 span').click(function(){
+        $('#popup_wrap').fadeIn()
+    })
+    $('#popup_close').click(function(){
+        $('#popup_wrap').fadeOut()
+    })
+    $('#popup_close2').click(function(){
+        $('#popup_wrap').fadeOut()
+    })
+})
